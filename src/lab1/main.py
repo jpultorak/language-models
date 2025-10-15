4ca90a7b9c6016934d1a91553962b7f2e71334e2
Jan Pultorak <janpultorak02@proton.me>
2025-10-15 22:07:17 +0200

    Initialize

---
+++ src/lab1/main.py
@@ -0,0 +1,24 @@
+from transformers import pipeline, set_seed
+
+generator = pipeline('text-generation', model='flax-community/papuGaPT2', device=0)
+
+print ('Model loaded')
+last_prompt = 'Dzisiaj na obiad zjemy kartofelki z'
+
+while True:
+    prompt = input().strip()
+    if not prompt:
+        prompt = last_prompt
+    g = generator(prompt,
+                  pad_token_id=generator.tokenizer.eos_token_id,
+                  max_new_tokens=100
+                  )[0]['generated_text']
+
+    print (g)
+    print (50 * '=')
+    print ()
+    last_prompt = prompt
+
+
+
+
