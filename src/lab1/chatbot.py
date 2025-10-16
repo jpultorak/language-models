7e6396a3cf47b67c4208455e3922d3aaa1a22946
Jan Pultorak <janpultorak02@proton.me>
2025-10-16 11:49:24 +0200

    Add improved version

--- src/lab1/chatbot.py
+++ src/lab1/chatbot.py
@@ -2,27 +2,34 @@ import re
 from transformers import pipeline, set_seed
 
 
-def build_prompt(query: str, history=None) -> str:
-    intro = (
-        "SCENA: Rozmowa dwóch osob, na temat małp.\n\n"
-        "Pytanie: Czy małpy lubią jeść patyki?\n"
-        "Odpowiedź: Nie, małpy wolą jeść banany.\n"
-    )
-
-    lines = []
-    if history:
-        for u, b in history[-1:]:
-            lines += [f"Pytanie: {u}", f"Odpowiedź: {b}"]
-    lines += [f"Pytanie: {query}", "Odpowiedź: "]
+def build_prompt(query, history):
+    header = "SCENA: krótka rozmowa. Odpowiadaj zwięźle i na temat.\n\n"
+    lines = [header]
+    for u, b in history[-1:]:
+        lines += [f"Pytanie: {u}", f"Odpowiedź: {b}"]
+    lines += [f"Pytanie: {query}", "Odpowiedź:"]
     return "\n".join(lines)
 
+import re
+
 def score(reply: str, user_query: str) -> float:
-    s = len(reply)
+    s = 0
+
+    words = reply.split()
+
+    consecutive_words = list(zip(words, words[1:]))
+    repeats = len(consecutive_words ) - len(set(consecutive_words))
+    s += 3 * repeats
+
+    runs = re.findall(r"\b(\w+)(?:\s+\1\b)+", reply.lower())
+    s += 9 * len(runs)
+
+    p_cnt = words.count("pytanie:")
+    s += 100 * min(p_cnt, 1)
     return s
 
-def process_output(text: str) -> str:
-    # text = text.split("\nUżytkownik:")[0].strip()
 
+def process_output(text: str) -> str:
     last_sentence_idx = max(text.rfind('.'), text.rfind('?'), text.rfind('!'))
     if last_sentence_idx != -1:
         return text[:last_sentence_idx+1]
@@ -47,17 +54,15 @@ def main():
 
         outs = generator(
             prompt,
-            max_new_tokens=50,
-            num_return_sequences=2,
+            min_new_tokens=10,
+            max_new_tokens=40,
+            num_return_sequences=10,
             return_full_text=False,
+            do_sample=True,
+            top_p=0.9, temperature=0.8,
         )
 
-        candidates = []
-        for o in outs:
-            text = o["generated_text"]
-            processed = process_output(text)
-            candidates.append(processed)
-
+        candidates = [process_output(o["generated_text"]) for o in outs]
         # print("=======DEBUG:\n ", candidates," \nEND DEBUG=======\n")
 
         reply = min(candidates, key=lambda r: score(r, query))
