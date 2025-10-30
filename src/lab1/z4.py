0948e4a2ec2cef3471642154864ff07d0de0e068
Jan Pultorak <janpultorak02@proton.me>
2025-10-30 13:48:31 +0100

    Add generic answers

--- src/lab1/z4.py
+++ src/lab1/z4.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from pathlib import Path
 from transformers import pipeline
 import re
+from lab1.sentence_probability import sentence_prob
+
 
 @dataclass(frozen=True)
 class Question:
@@ -10,7 +12,7 @@ class Question:
     answer: str
 
     def __str__(self):
-        return f"Q: {self.question}A: {self.answer}"
+        return f"Q: {self.question}\nA: {self.answer}"
 
 
 def read_questions(path: Path) -> list[Question]:
@@ -21,7 +23,7 @@ def read_questions(path: Path) -> list[Question]:
 
     with q_path.open() as q_file, a_path.open() as a_file:
         for q, a in zip(q_file, a_file):
-            questions.append(Question(question=q, answer=a))
+            questions.append(Question(question=q.rstrip(), answer=a.rstrip()))
 
     return questions
 
@@ -37,9 +39,10 @@ def gen(prompt: str, generator, max_tokens = 10) -> str:
         do_sample=False,
         max_new_tokens=max_tokens,
         return_full_text=False,
+        repetition_penalty=1.5,
     )[0]["generated_text"]
 
-    return g
+    return g.strip()
 
 
 def answer_year(q: str, generator) -> int | None:
@@ -58,16 +61,25 @@ def answer_century(q: str, generator) -> int | None:
     return 2137
 
 def generic_answer(q: str, generator):
-    prompt = f"Odpowiedź na pytanie: {q} to: "
+
+    prompt = f"Pytanie: {q}\nOdpowiedz krótko (1-3 słowa).\nOdpowiedź:"
     return gen(prompt, generator)
 
-# def answer_yes_no(q: Question, generator):
-#     prompt = f"Odpowiedz jednym słowem (tak/nie).\nPytanie: {q.question}\nOdpowiedź:"
-#     g = generator(
-#         prompt, pad_token_id=generator.tokenizer.eos_token_id, max_new_tokens=10, return_full_text=False,
-#     )[0]["generated_text"]
-#
-#     return g
+def answer_yes_no(q: str):
+    prompt = f"Pytanie: {q}\nOdpowiedz jednym słowem (tak/nie).\nOdpowiedź:"
+
+    s_yes = sentence_prob(prompt + " tak")
+    s_no = sentence_prob(prompt + " nie")
+
+    return "tak" if s_yes >= s_no else "nie"
+
+def answer_x_y(q: str, x: str, y: str):
+    prompt = f"Pytanie: {q}\nOdpowiedz jednym słowem f({x}/{y}).\nOdpowiedź:"
+
+    s_x = sentence_prob(prompt + f" {x}")
+    s_y = sentence_prob(prompt + f" {y}")
+
+    return x if s_x >= s_y else y
 
 if __name__ == "__main__":
     repo_root = Path(__file__).resolve().parents[2]
@@ -76,7 +88,7 @@ if __name__ == "__main__":
 
     generator = load_model()
 
-    for q in qs[100:400]:
+    for q in qs[330:360]:
         words = q.question.lower().split()
         answer = None
         if words[0:2] == ["w", "którym"] and words[2] in ("roku", "wieku"):
@@ -88,15 +100,28 @@ if __name__ == "__main__":
                 print("PYTANIE O WIEK!")
                 answer = answer_century(q.question, generator)
 
-        if answer is not None:
-            print(f"{q}LM answer: {answer}")
-            print("====================================")
+        last_czy = -1
+        for (i, w) in enumerate(words):
+            if w == "czy":
+                last_czy = i
 
-        # if answer is None:
-        #     answer = generic_answer(q.question, generator)
-        #
-        # print(f"{q}LM answer: {answer}")
-        # print("====================================")
+        if  last_czy != -1:
+            if last_czy == 0:
+                print("YES or NO QUESTION")
+                answer = answer_yes_no(q.question)
+            else:
+                x = words[last_czy-1]
+                y = words[last_czy+1]
+                if y[-1] == '?':
+                    y = y[:-1]
+                print(f"{x} or {y} QUESTION!")
+                answer = answer_x_y(q.question, x, y)
 
+        if answer is None:
+            answer = generic_answer(q.question, generator)
+
+        print(f"{q}\nLM answer: {answer}")
+        print("====================================")
+        print()
 
 
