a24b47fd87ef711eba0540ac2ea5bf8a4fde30ed
Jan Pultorak <janpultorak02@proton.me>
2025-10-30 10:58:34 +0100

    Add first version

--- src/lab1/z4.py
+++ src/lab1/z4.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
+from transformers import pipeline
 
 
 @dataclass(frozen=True)
@@ -24,10 +25,32 @@ def read_questions(path: Path) -> list[Question]:
 
     return questions
 
+def load_model(model_name = "eryk-mazus/polka-1.1b"):
+    generator = pipeline("text-generation", model=model_name, device=0)
+    print("Model loaded")
+    return generator
+
+
+def answer_question(q: Question, generator):
+
+    prompt = f"Odpowiedź na pytanie: {q.question} to: "
+    g = generator(
+        prompt, pad_token_id=generator.tokenizer.eos_token_id, max_new_tokens=15
+    )[0]["generated_text"]
+
+    return g
 
 if __name__ == "__main__":
     repo_root = Path(__file__).resolve().parents[2]
     z4_path = repo_root / "datasets" / "p1" / "z4"
     qs = read_questions(z4_path)
-    for q in qs:
-        print(q)
+
+    generator = load_model()
+
+    for q in qs[:5]:
+        print(f"{q}LM answer: {answer_question(q, generator)}")
+        print("-----------------------------------")
+
+
+
+
